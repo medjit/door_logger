7b1567645a511a7eb2e703d6d83b21129c31ebda
medjit <medjit.saitov@abv.bg>
2022-07-17 19:26:57 +0300

    Initial implement of Blynk legacy connection

--- door_logger.ino
+++ door_logger.ino
@@ -2,8 +2,13 @@
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <AsyncElegantOTA.h>
+#include <BlynkSimpleEsp8266.h>
 #include "Credentials.h"
 
+#define BLYNK_TEMPLATE_ID   DOOR_LOGGER_BLYNK_TEMPLATE_ID
+#define BLYNK_DEVICE_NAME   DOOR_LOGGER_BLYNK_DEVICE_NAME
+#define BLYNK_AUTH_TOKEN    DOOR_LOGGER_BLYNK_AUTH_TOKEN
+char auth[] = BLYNK_AUTH_TOKEN;
 const char* ssid = DOOR_LOGGER_SSID;
 const char* password = DOOR_LOGGER_PASS;
 
@@ -13,6 +18,7 @@ AsyncWebServer server(80);
 
 void setup(void) {
   delay(2000);
+  Blynk.begin(auth, ssid, password, DOOR_LOGGER_BLYNK_ADDRESS, 8080);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
@@ -24,6 +30,9 @@ void setup(void) {
   server.begin();
 }
 
+byte count = 0;
 void loop(void) {
-  
+  Blynk.run();
+  Blynk.virtualWrite(V0, count++);
+  delay(1000);
 }
